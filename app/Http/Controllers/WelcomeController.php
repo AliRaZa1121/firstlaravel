0eff9b9abfa5943153afbfeacefd0d568285853e
Ali RaZa <62483196+AliRaZa1121@users.noreply.github.com>
2020-11-06 08:47:05 +0500

    seccomit

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class WelcomeController extends Controller{
+    public function welcome(){
+        echo "Welcome world"; //mgr controller me echo krna munasib nhi
+    }
+}
